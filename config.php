6fe8d535b6749a683f4d89e0963cb40553210e09
sanchescom <sanches.com@mail.ru>
2018-03-15 19:49:41 +0300

    added docker and some libs

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config = [
+	'size' => [
+		'alias' => 's',
+		'help' => 'Size of the shapes',
+	],
+];
--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
-require_once "../vendor/autoload.php";
+require_once ("vendor/autoload.php");
+
+class Application
+{
+	private $config;
+	private $request;
+
+	public function __construct()
+	{
+		$this->config  = \Noodlehaus\Config::load('config.json');
+		$this->request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+	}
+
+
+	public function run()
+	{
+
+	}
+}
+
+
+
+
